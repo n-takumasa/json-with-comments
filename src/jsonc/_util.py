febcd3429e7c04ffe8ccc8f5fd9ecc73bfa1093d
Jonathan Dung <jonathandung@yahoo.com>
2026-03-31 14:04:32 +0800

    Precompile regular expressions

--- src/jsonc/_util.py
+++ src/jsonc/_util.py
@@ -2,7 +2,10 @@ from __future__ import annotations
 
 import re
 
-_REMOVE_C_COMMENT = r"""
+def _make_pattern(s: str, /):
+    return re.compile(s, re.DOTALL | re.VERBOSE)
+
+_REMOVE_C_COMMENT = _make_pattern(r"""
     ( # String Literal
         \"(?:\\.|[^\\\"])*?\"
     )
@@ -12,46 +15,36 @@ _REMOVE_C_COMMENT = r"""
         |
         \/\/[^\r\n]*?(?:[\r\n])
     )
-    """
+    """)
 
 
-_REMOVE_TRAILING_COMMA = r"""
+_REMOVE_TRAILING_COMMA = _make_pattern(r"""
     ( # String Literal
         \"(?:\\.|[^\\\"])*?\"
     )
     | # Right Brace without Trailing Comma & Spaces
     ,\s*([\]}])
-"""
+""")
 
 
-_ADD_TRAILING_COMMA = r"""
+_ADD_TRAILING_COMMA = _make_pattern(r"""
     ( # String Literal
         \"(?:\\.|[^\\\"])*?\"
     )
     | # Don't match opening braces to avoid {,}
     ((?<=\")|[^,\[{\s])
     (?=\s*([\]}]))
-"""
+""")
 
 
 def _remove_c_comment(text: str) -> str:
     if text[-1] != "\n":
         text = text + "\n"
-    return re.sub(
-        _REMOVE_C_COMMENT,
-        lambda x: x.group(1),
-        text,
-        flags=re.DOTALL | re.VERBOSE,
-    )
+    return _REMOVE_C_COMMENT.sub(lambda x: x.group(1), text)
 
 
 def _remove_trailing_comma(text: str) -> str:
-    return re.sub(
-        _REMOVE_TRAILING_COMMA,
-        lambda x: x.group(1) or x.group(2),
-        text,
-        flags=re.DOTALL | re.VERBOSE,
-    )
+    return _REMOVE_TRAILING_COMMA.sub(lambda x: x.group(1) or x.group(2), text)
 
 
 def _add_trailing_comma(text: str) -> str:
